bf8861708cbc9ea95cbdf34da8b788245a6d4526
srikar5524 <chsrikar436@gmail.com>
2024-06-23 10:08:46 +0530

    Add files via upload

---
+++ CodeAlpha_task_imagr_gallery/script.js
@@ -0,0 +1,44 @@
+function fun1(){
+    document.getElementById("photo").src="./photos/1.jpg"
+}
+function fun2(){
+    document.getElementById("photo").src="./photos/2.jpg"
+}
+function fun3(){
+    document.getElementById("photo").src="./photos/3.jpg"
+}
+function fun4(){
+    document.getElementById("photo").src="./photos/4.jpg"
+}
+function fun5(){
+    document.getElementById("photo").src="./photos/5.jpg"
+}
+function fun6(){
+    document.getElementById("photo").src="./photos/6.jpg"
+}
+function fun7(){
+    document.getElementById("photo").src="./photos/7.jpg"
+}function fun8(){
+    document.getElementById("photo").src="./photos/8.jpg"
+}
+function fun9(){
+    document.getElementById("photo").src="./photos/9.jpg"
+}
+function fun10(){
+    document.getElementById("photo").src="./photos/10.jpg"
+}
+function fun11(){
+    document.getElementById("photo").src="./photos/11.jpg"
+}
+function fun12(){
+    document.getElementById("photo").src="./photos/12.jpg"
+}
+function fun13(){
+    document.getElementById("photo").src="./photos/13.jpg"
+}
+function fun14(){
+    document.getElementById("photo").src="./photos/14.jpg"
+}
+function fun15(){
+    document.getElementById("photo").src="./photos/15.jpg"
+}
